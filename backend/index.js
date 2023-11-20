0de6d84c2eadff0c34a11a28e4dbe03f14c8aeda
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-11-20 22:54:12 +0530

    setup cron job

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const express = require("express");
 const daoRoutes = require("./routes/dao");
+const claim = require("./contracts");
+const cron = require("node-cron");
 
 const app = express();
 
@@ -12,6 +14,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(daoRoutes);
+
+//RUNS EVERY HOUR
+cron.schedule("0 * * * *", async () => {
+  await claim();
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
