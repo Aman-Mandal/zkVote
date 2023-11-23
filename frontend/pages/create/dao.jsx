b7a6a1f162f84edbfb92fe7f5f7b5bb841bcf328
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-11-23 19:31:01 +0530

    added functionality to deploy governance contract from UI

--- frontend/pages/create/dao.jsx
+++ frontend/pages/create/dao.jsx
@@ -1,34 +1,41 @@
-import CreateStep1 from '@/components/DAO/CreateStep1';
-import CreateStep2 from '@/components/DAO/CreateStep2';
-import CreateStep3 from '@/components/DAO/CreateStep3';
-import VerticalStepper from '@/components/DAO/Stepper';
-import Loader from '@/components/Layout/Loader';
-import { DAO_STEPS, SERVER_URL } from '@/constants';
-import { useWeb3Modal } from '@web3modal/wagmi/react';
-import React, { useState } from 'react';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import { toast, ToastContainer } from 'react-toastify';
-import { useAccount } from 'wagmi';
-import 'react-toastify/dist/ReactToastify.css';
+import CreateStep1 from "@/components/DAO/CreateStep1";
+import CreateStep2 from "@/components/DAO/CreateStep2";
+import CreateStep3 from "@/components/DAO/CreateStep3";
+import VerticalStepper from "@/components/DAO/Stepper";
+import Loader from "@/components/Layout/Loader";
+import {
+  DAO_STEPS,
+  GovernorABI,
+  GovernorBytecode,
+  SERVER_URL,
+  TimelockContract,
+} from "@/constants";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import React, { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { toast, ToastContainer } from "react-toastify";
+import { useAccount } from "wagmi";
+import { ContractFactory, ethers } from "ethers";
+import "react-toastify/dist/ReactToastify.css";
 
 const CreateDao = () => {
   const [page, setPage] = useState(0);
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    category: '',
-    ethereumAddress: '',
-    polygonAddress: '',
-    zkEVMAddress: '',
-    tokenName: '',
-    daoAddress: '',
-    threshold: '',
-    twitter: '',
-    discord: '',
-    github: '',
-    linkedIn: '',
-    telegram: '',
-    logo: '',
+    name: "",
+    description: "",
+    category: "",
+    ethereumAddress: "",
+    polygonAddress: "",
+    zkEVMAddress: "",
+    tokenName: "",
+    daoAddress: "",
+    threshold: "",
+    twitter: "",
+    discord: "",
+    github: "",
+    linkedIn: "",
+    telegram: "",
+    logo: "",
   });
   const [isLoading, setIsLoading] = useState(false);
 
@@ -37,26 +44,11 @@ const CreateDao = () => {
 
   const PageDisplay = () => {
     if (page === 0) {
-      return (
-        <CreateStep1
-          formData={formData}
-          setFormData={setFormData}
-        />
-      );
+      return <CreateStep1 formData={formData} setFormData={setFormData} />;
     } else if (page === 1) {
-      return (
-        <CreateStep2
-          formData={formData}
-          setFormData={setFormData}
-        />
-      );
+      return <CreateStep2 formData={formData} setFormData={setFormData} />;
     } else if (page === 2) {
-      return (
-        <CreateStep3
-          formData={formData}
-          setFormData={setFormData}
-        />
-      );
+      return <CreateStep3 formData={formData} setFormData={setFormData} />;
     }
   };
 
@@ -68,14 +60,39 @@ const CreateDao = () => {
     if (page > 0) setPage((currPage) => currPage - 1);
   };
 
+  const deployGovernance = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const signer = provider.getSigner();
+    const factory = new ContractFactory(GovernorABI, GovernorBytecode, signer);
+
+    const contract = await factory.deploy(
+      formData.ethereumAddress,
+      TimelockContract,
+      "100000000000000000"
+    );
+
+    await contract.deployed();
+
+    console.log({ address: contract.address });
+
+    return contract.address;
+  };
+
   const submitProjectHandler = async (e) => {
     try {
       setIsLoading(true);
       e.preventDefault();
+
+      let daoContract;
+
+      if (formData.daoAddress === "") {
+        daoContract = await deployGovernance();
+      }
       const response = await fetch(`${SERVER_URL}/create`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           name: formData.name,
@@ -83,10 +100,11 @@ const CreateDao = () => {
           category: formData.category,
           tokenName: formData.tokenName,
           tokenAddresses: [
-            { chain: 'Ethereum', address: formData.ethereumAddress },
-            { chain: 'zkEvm', address: formData.zkEVMAddress },
+            { chain: "Ethereum", address: formData.ethereumAddress },
+            { chain: "zkEvm", address: formData.zkEVMAddress },
           ],
-          daoContract: formData.daoAddress,
+          daoContract:
+            formData.daoAddress === "" ? daoContract : formData.daoAddress,
           socials: {
             twitter: formData.twitter,
             github: formData.github,
@@ -99,73 +117,75 @@ const CreateDao = () => {
 
       const data = await response.json();
 
-      console.log('data is', data);
+      console.log("data is", data);
 
       setIsLoading(false);
-      toast.success('DAO added successfully!');
+      toast.success("DAO added successfully!");
     } catch (error) {
       console.log(error);
       setIsLoading(false);
-      toast.error('Some error occurred!');
+      toast.error("Some error occurred!");
     }
   };
 
   return (
-    <div className='min-h-screen bg-[#111111] pt-20  font-Avenir'>
-      <div className='flex  text-white'>
-        <div className='flex-[0.35] flex flex-col px-20 py-14 border-r-[0.5px]  border-[#2E2E2E]'>
-          <div className='mb-10'>
-            <p className='text-3xl font-semibold mb-1 text-[#f2f2f2]'>
+    <div className="min-h-screen bg-[#111111] pt-20  font-Avenir">
+      <div className="flex  text-white">
+        <div className="flex-[0.35] flex flex-col px-20 py-14 border-r-[0.5px]  border-[#2E2E2E]">
+          <div className="mb-10">
+            <p className="text-3xl font-semibold mb-1 text-[#f2f2f2]">
               Create DAO
             </p>
-            <p className='text-sm text-[#707070]'>
+            <p className="text-sm text-[#707070]">
               Setup your DAO in few simple steps.
             </p>
           </div>
-          <VerticalStepper
-            steps={DAO_STEPS}
-            page={page}
-          />
+          <VerticalStepper steps={DAO_STEPS} page={page} />
         </div>
         <form
           onSubmit={submitProjectHandler}
-          className=' w-[400px] flex-[0.5]  py-8 px-10  mb-10'>
+          className=" w-[400px] flex-[0.5]  py-8 px-10  mb-10"
+        >
           {PageDisplay()}
-          <div className='flex justify-between mt-8 items-center'>
+          <div className="flex justify-between mt-8 items-center">
             {page !== 0 ? (
               <button
-                type='button'
+                type="button"
                 onClick={previousPageHandler}
-                className='flex items-center gap-2 text-gray-400 hover:text-white'>
+                className="flex items-center gap-2 text-gray-400 hover:text-white"
+              >
                 <IoIosArrowBack />
                 <p>Previous</p>
               </button>
             ) : (
-              <p className='text-[#181818]'>.</p>
+              <p className="text-[#181818]">.</p>
             )}
 
             {page === 2 ? (
               <div>
                 {isConnected ? (
                   <button
-                    type='submit'
-                    className='bg-[#292929] hover:bg-[#333333] text-sm py-2 w-[150px] rounded-md'>
-                    {isLoading ? <Loader inComp={true} /> : 'Submit'}
+                    type="submit"
+                    className="bg-[#292929] hover:bg-[#333333] text-sm py-2 w-[150px] rounded-md"
+                  >
+                    {isLoading ? <Loader inComp={true} /> : "Submit"}
                   </button>
                 ) : (
                   <button
-                    type='button'
+                    type="button"
                     onClick={() => open()}
-                    className='bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md'>
+                    className="bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md"
+                  >
                     Connect
                   </button>
                 )}
               </div>
             ) : (
               <button
-                type='button'
+                type="button"
                 onClick={nextPageHandler}
-                className='flex items-center gap-2 text-gray-400 hover:text-white'>
+                className="flex items-center gap-2 text-gray-400 hover:text-white"
+              >
                 <p>Next</p>
                 <IoIosArrowForward />
               </button>
@@ -173,7 +193,7 @@ const CreateDao = () => {
           </div>
         </form>
       </div>
-      <ToastContainer theme='dark' />
+      <ToastContainer theme="dark" />
     </div>
   );
 };
